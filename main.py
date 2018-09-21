e82279b8a136a4c3a07d429ec590a5a058aea402
Sangeetha Rani <sangeetha.sabarinath@gmail.com>
2018-09-20 19:48:22 -0500

    user-signup working, yet to implement templates

--- main.py
+++ main.py
@@ -12,19 +12,19 @@ data_form = """
     <h1>Signup</h1>
     <form method='POST'>
         <label>Username
-            <input name="user-name" type="text" value='{user_name}' />
+            <input name="username" type="text" value='{username}' />
         </label>
         <p class="error">{name_error}</p>
         <label>Password
-            <input name="user-pswd" type="password" value='{password}'/>
+            <input name="userpswd" type="password" value='{userpswd}'/>
         </label>
         <p class="error">{pswd_error}</p>
         <label>Verify Password
-            <input name="verify-pswd" type="password" value='{v_pswd}' />
+            <input name="verifypswd" type="password" value='{verifypswd}' />
         </label>
         <p class="error">{v_pswd_error}</p>
         <label>Email(optional)
-            <input name="user-email" type="text" value='{email}' />
+            <input name="useremail" type="text" value='{useremail}' />
         </label>
         <p class="error">{email_error}</p>
         <input type="submit" value="Submit" />
@@ -34,69 +34,51 @@ data_form = """
 
 @app.route('/validate-data')
 def display_data_form():
-    return data_form.format(user_name='', name_error='', 
-    password='', pswd_error='', v_pswd='', v_pswd_error='', 
-    email='', email_error='')
-
-def is_alpha(str_input):
-    try:
-        (str_input).isalpha()
-        return True
-    except ValueError:
-        return False
+    return data_form.format(username='', name_error='', 
+    userpswd='', pswd_error='', verifypswd='', v_pswd_error='', 
+    useremail='', email_error='')
 
 
 @app.route('/validate-data', methods=['POST'])
 def validate_data():
     
-    user_name = request.form['user_name']
-    user_pswd = request.form['password']
-    user_v_pswd = request.form['v_pswd']
-    user_email = request.form['email']
+    username = request.form['username']
+    userpswd = request.form['userpswd']
+    verifypswd = request.form['verifypswd']
+    useremail = request.form['useremail']
 
     name_error = ''
     pswd_error = ''
     v_pswd_error = ''
     email_error = ''
 
-    if not is_alpha(user_name):
+    if len(username) < 3 or len(username) > 20 or not username.isalpha():
         name_error = "That's not a valid username"
-        user_name = ''
-    else:
-        if len(user_name) < 3 or len(user_name) > 20:
-            name_error = "That's not a valid username"
-            user_name = ''
+        username = ''
 
-    if not is_alpha(user_pswd):
+    if len(userpswd) < 3 or len(username) > 20 or not userpswd.isalpha():
         pswd_error = "That's not a valid password"
-        user_pswd = ''
-    else:
-        if len(user_pswd) < 3 or len(user_name) > 20:
-            pswd_error = "That's not a valid password"
-            user_pswd = ''
+        userpswd = ''
         
-    if not is_alpha(user_v_pswd):
+    if len(verifypswd) < 3 or len(verifypswd) > 20 or not verifypswd.isalpha() or userpswd != verifypswd:
         v_pswd_error = "Passwords don't match"
-        user_v_pswd = ''
-    else:
-        if len(user_v_pswd) < 3 or len(user_v_pswd) > 20 or user_pswd != user_v_pswd:
-            v_pswd_error = "Passwords don't match"
-            user_v_pswd = ''
+        verifypswd = ''
 
-    if not is_alpha(user_email):
-        email_error = "That's not a valid email"
-        user_email = ''
+    
+    if useremail == '':
+        useremail = ''
     else:
-        if '@' not in user_email or '.' not in user_email:
-            email_error = "That's not a valid email"
-            user_email = ''
+        if useremail != '':
+            if '@' not in useremail or '.' not in useremail:
+                email_error = "That's not a valid email"
+                useremail = ''
 
     if not name_error and not pswd_error and not v_pswd_error and not email_error:
-        return "<h1>Welcome " + user_name + "!</h1>"
+        return "<h1>Welcome " + username + "!</h1>"
     else:
         return data_form.format(name_error=name_error, 
-        pswd_error=pswd_error, v_pswd_error=v_pswd_error, email_error=email_error, user_name=user_name,
-        user_pswd=user_pswd, user_v_pswd=user_v_pswd, user_email=user_email)
+        pswd_error=pswd_error, v_pswd_error=v_pswd_error, email_error=email_error, username=username,
+        userpswd=userpswd, verifypswd=verifypswd, useremail=useremail)
 
 @app.route("/")
 def index():
